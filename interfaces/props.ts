fb12b7a198977a24952e1d9212b9415b0f3adabb
Christian Sosa <sosa.christian.agustin@gmail.com>
2022-09-17 09:26:32 -0300

    Revert "stack lambdas"
    This reverts commit f93fdcb8774c03d61e8a157241cc260460653f57.

--- interfaces/props.ts
+++
@@ -1,9 +0,0 @@
-import { StackProps } from "aws-cdk-lib";
-
-export default interface AppProps extends StackProps{
-    stage: string;
-    client: string;
-    project: string;
-    snsTopic: string;
-    bucket: string;
-}
